d36d09048cae39c784458790dc9bcd74f62fe0da
Igor Lisakov <igor.lisakov@sigma.software>
2020-10-30 14:20:37 +0200

    Open file in separate tab.

--- src/components/FileDownload.vue
+++ src/components/FileDownload.vue
@@ -19,10 +19,12 @@
                 axios.get(this.url, { responseType: 'blob' })
                     .then(response => {
                         const blob = new Blob([response.data], { type: 'image/png' })
-                        const link = document.createElement('a')
-                        link.href = URL.createObjectURL(blob)
-                        link.download = this.name
-                        link.click()
+                        //const link = document.createElement('a')
+                        //link.href = URL.createObjectURL(blob)
+                        //link.download = this.name
+                        //link.click()
+                        const link = URL.createObjectURL(blob)
+                        window.open(link, '_blank')
                         URL.revokeObjectURL(link.href)
                     }).catch(console.error)
             }
